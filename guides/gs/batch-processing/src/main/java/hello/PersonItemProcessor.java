07e9ea7477467c9ea30114773cb82d08d080ce19
Clemens Quoss <clemens@quoss.de>
2017-06-13 20:36:44 +0200

    next iteration ...

---
+++ guides/gs/batch-processing/src/main/java/hello/PersonItemProcessor.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.batch.item.ItemProcessor;
+
+public class PersonItemProcessor implements ItemProcessor<Person, Person> {
+
+	private static final Logger log = LoggerFactory.getLogger(PersonItemProcessor.class);
+	
+	
+	
+}
