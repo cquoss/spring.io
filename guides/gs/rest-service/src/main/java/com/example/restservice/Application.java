edd4e3b7324640af50e266b32a21f20e3d0ea7ee
Clemens Quoss <clemens@quoss.de>
2020-11-24 23:28:41 +0100

    Share current progress

--- guides/gs/rest-service/src/main/java/com/example/restservice/Application.java
+++ guides/gs/rest-service/src/main/java/com/example/restservice/Application.java
@@ -1,7 +1,9 @@
-package hello;
+package com.example.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import hello.Application;
 
 @SpringBootApplication
 public class Application {
